c95824b52a077dfea6b3e3ed9350987eaa2ada61
MrSalim86 <cph-ms1068@cphbusiness.dk>
2024-11-18 17:14:05 +0100

    start

---
+++ OrderManagementServiceTests/UnitTest/OrderUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementServiceTests.UnitTest
+{
+    public class OrderUnitTest
+    {
+        [Fact]
+        public async Task Test123()
+        {
+            int number1 = 5;
+            int number2 = 10;
+
+            int result = (number1 + number2);
+
+            Assert.Equal(15, result);
+        }
+    }
+}
