0c15d1812458097d9a5d6b6c80b73c87961093fd
MrSalim86 <cph-ms1068@cphbusiness.dk>
2025-01-05 13:31:47 +0100

    update2

--- OrderManagementService/obj/Debug/net8.0/OrderManagementService.AssemblyInfo.cs
+++ OrderManagementService/obj/Debug/net8.0/OrderManagementService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagementService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2b20214de11192ac05f3572561c6c17eca725d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370d68f5469c0e26b1ea911a26b3584487dd5449")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagementService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagementService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OrderManagementServiceTests/obj/Debug/net8.0/OrderManagementServiceTests.AssemblyInfo.cs
+++ OrderManagementServiceTests/obj/Debug/net8.0/OrderManagementServiceTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagementServiceTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2b20214de11192ac05f3572561c6c17eca725d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370d68f5469c0e26b1ea911a26b3584487dd5449")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagementServiceTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagementServiceTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
